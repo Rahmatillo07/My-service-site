ef37e58517c1db9bb6e6d6553d3e06295e57ed5a
Rahmatillo <Rahmatillo@gmail.com>
2025-05-12 16:31:44 +0500

    all errors

--- portfolio_landing_page/settings.py
+++ portfolio_landing_page/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = 'media/'
